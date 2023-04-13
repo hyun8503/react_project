6132379b6f61e3541aef43c9f6a59f3c0e52dc16
Jane <hyun8503@gmail.com>
2023-04-13 15:36:36 +0900

    첫 커밋

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+
+
+const border = {
+  marginBottom:'50px'
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={border} >
+      <Header/>
+      <Main/>
+      <Footer />
     </div>
   );
+
 }
 
 export default App;
